154cc6935ea477605f8a5512c793dcf134f0172d
Luqman Abdulwasii <luqmanabdulwasii@gmail.com>
2022-03-10 16:51:00 +0100

    added contructor arguments

--- HelloWorld.sol
+++ HelloWorld.sol
@@ -1,17 +1,21 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-contract ZuriHello {
+contract HelloWorld {
+    
     string public message;
+    string public name;
 
-    constructor() {
+    constructor(string memory _name) {
+        name = _name;
     }
 
-    function storeMessage() public {
-        message = "Hello world!";
+    function storeMessage(string memory studentName) public {
+        message = "Hello ";
+        name = studentName;
     }
 
     function returnMessage() public view returns(string memory) {
-        return message;
+        return string(abi.encodePacked(message, name));
     }
 }
